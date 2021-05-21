b49eb971b0629d1677e0510e488ff92829e94a4a
Данила Сар <mr.sda2014@yandex.ru>
2021-05-21 10:27:33 +0300

    Project

---
+++ CarRentalDB/Models/Car.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DB.Models
+{
+    public class Cars
+    {
+        public long ID { get; set; }
+        public int Registration_number { get; set; }
+        public string Return_mark { get; set; }
+        public DateTime Date_of_last_maintenance { get; set; }
+        public string Special_marks { get; set; }
+        public int Rental_day_price { get; set; }
+        public int Car_price { get; set; }
+        public int Mileage { get; set; }
+        public int Body_number { get; set; }
+        public int Engine_number { get; set; }
+        public DateTime Year_of_release { get; set; }
+        public DbSet<Car_Brands> Brand_code { get; set; }
+        public DbSet<Rental_services> Rental_code { get; set; }
+
+    }
+}
---
+++ CarRentalDB/Models/CarBrand.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DB.Models
+{
+    public class Car_Brands
+    {
+        public long ID { get; set; }
+        public string Description { get; set; }
+        public string Technical_specifications { get; set; }
+        public string Name { get; set; }
+    }
+}
